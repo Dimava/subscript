e5f1291d80967c029784e52703fcc3cb835e06ed
Dimava <dimava2@ya.ru>
2025-10-30 03:13:19 +0300

    atash

--- v2/src/parse.ts
+++ v2/src/parse.ts
@@ -1,14 +1,14 @@
 import { SPACE } from "./const.js"
 
-// current string, index and collected ids
+/** current string, index and collected ids */
 export let idx, cur, lookup = []
 
-// no handling tagged literals since easily done on user side with cache, if needed
+/** no handling tagged literals since easily done on user side with cache, if needed */
 export function parse(s) {
   return (idx = 0, cur = s, s = expr(), cur[idx] ? err() : s || '')
 }
 
-// display error
+/** display error */
 export function err(msg = 'Bad syntax',
   lines = cur.slice(0, idx).split('\n'),
   last = lines.pop()
@@ -18,18 +18,18 @@ export function err(msg = 'Bad syntax',
   throw EvalError(`${msg} at ${lines.length}:${last.length} \`${idx >= 108 ? '…' : ''}${before}┃${after}\``, 'font-weight: bold')
 }
 
-// advance until condition meets
+/** advance until condition meets */
 export function next(is, from = idx, l) {
   while (l = is(cur.charCodeAt(idx))) idx += l
   return cur.slice(from, idx)
 }
 
-// advance n characters
+/** advance n characters */
 export function skip() {
   return cur[idx++]
 }
 
-// a + b - c
+/** a + b - c */
 export function expr(prec = 0, end) {
   let cc, token, newNode, fn
 
@@ -50,13 +50,13 @@ export function expr(prec = 0, end) {
   return token
 }
 
-// skip space chars, return first non-space character
+/** skip space chars, return first non-space character */
 export function space(cc) {
   while ((cc = cur.charCodeAt(idx)) <= SPACE) idx++;
   return cc
 }
 
-// parse identifier (configurable)
+/** parse identifier (configurable) */
 export const id = parse.id = function(c) {
   return (
     (c >= 48 && c <= 57) || // 0..9
@@ -67,11 +67,13 @@ export const id = parse.id = function(c) {
   )
 }
 
-// operator/token lookup table
-// lookup[0] is id parser to let configs redefine it
+/**
+ * operator/token lookup table
+ * lookup[0] is id parser to let configs redefine it
+ */
 
 
-// create operator checker/mapper (see examples)
+/** create operator checker/mapper (see examples) */
 export function token(
   op,
   prec = SPACE,
@@ -95,21 +97,21 @@ export function token(
   }
 }
 
-// right assoc is indicated by negative precedence (meaning go from right to left)
+/** right assoc is indicated by negative precedence (meaning go from right to left) */
 export function binary(op, prec, right = false) {
   return token(op, prec, function(a, b) {
     return a && (b = expr(prec - (right ? .5 : 0))) && [op, a, b]
   })
 }
 
-// post indicates postfix rather than prefix operator
+/** post indicates postfix rather than prefix operator */
 export function unary(op, prec, post) {
   return token(op, prec, function(a) {
     return post ? (a && [op, a]) : (!a && (a = expr(prec - .5)) && [op, a])
   })
 }
 
-// FIXME: skips means ,,, ;;; are allowed
+/** FIXME: skips means ,,, ;;; are allowed */
 export function nary(op, prec, skips) {
   token(op, prec,
     function(a, b) {
@@ -124,16 +126,20 @@ export function nary(op, prec, skips) {
   )
 }
 
-// register (a), [b], {c} etc groups
-// FIXME: add "Unclosed paren" error
+/**
+ * register (a), [b], {c} etc groups
+ * FIXME: add "Unclosed paren" error
+ */
 export function group(op, prec) {
   return token(op[0], prec, function(a) {
     return (!a && [op, expr(0, op.charCodeAt(1))])
   })
 }
 
-// register a(b), a[b], a<b> etc,
-// NOTE: we make sure `null` indicates placeholder
+/**
+ * register a(b), a[b], a<b> etc,
+ * NOTE: we make sure `null` indicates placeholder
+ */
 export function access(op, prec) {
   return token(op[0], prec, function(a) {
     return (a && [op, a, expr(0, op.charCodeAt(1)) || null])
